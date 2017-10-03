bef252d7808c00d90fc2dc0dbb7b503361d2035e
Ashish Tibrewal <at12060@my.bristol.ac.uk>
2017-10-03 18:53:11 +0530

    Added functionality for inserting company name in the CV note through an argument passed to the compilation script.

--- compile.sh
+++ compile.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
 # Script to compile CV LaTeX source files
+# First argument ($1): Name of company
 
 ##################################################
 # Compile LaTeX files using the XeLaTeX compiler #
 ##################################################
-
-xelatex "CV.tex"
+# If an argument, i.e. a company name is given, then pass it as an argument to
+# the xelatex binary
+if [ "$#" -eq 1 ]; then
+  xelatex "\def\company{$1} \input{CV.tex}"
+else
+  xelatex "CV.tex"
+fi
 
 ##################################################
 # Open file using the default PDF viewer (Adobe) #
